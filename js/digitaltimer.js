6da20cbf8de7628c81e673adcd40ce4a0f17d1a0
Christopher Riffle <criffle@Christophers-Mac-Pro.local>
2019-04-08 16:20:18 -0400

    Fixed timer to match provided html

--- js/digitaltimer.js
+++ js/digitaltimer.js
@@ -2,7 +2,7 @@ class Timer{
     constructor(){
       this.interval = null;
       this.timer = 0;    
-      document.querySelector(".timer h1").textContent = "0.00";
+ 
       this.startButton = null;
       this.resetButton = null;
     }
@@ -16,30 +16,44 @@ class Timer{
         this.resetButton = button;
         this.resetButton .addEventListener("click", () => {this.onReset()});
     }
-  
-    onTimerClick(){
+    
+    updateDigits(value){
+        let str = value.split(".");
+        let sTens = str[0].length > 1 ? str[0].substring(0, 1) : "0";
+        let sOnes = str[0].length > 1 ? str[0].substring(1, 2) : str[0];
+        let test = str[0].length;
+        
+        document.getElementById("secondTens").textContent = sTens;
+        document.getElementById("secondOnes").textContent = sOnes;
+        document.getElementById("msHundreds").textContent = str[1].substring(0,1);
+        document.getElementById("msTens").textContent = str[1].substring(1,2);
+    }
 
+    onTimerClick(){
         this.timer = 0;
         let timerEvent = this;
         this.startButton.disabled = true;
         
         this.interval  = setInterval(function(){
           timerEvent.timer += 10;
-          document.querySelector(".timer h1").textContent = (timerEvent.timer / 1000).toFixed(2);
+          timerEvent.updateDigits((timerEvent.timer / 1000).toFixed(2));
           if (timerEvent.timer >= 10000){
             timerEvent.startButton.disabled = false;
-            document.querySelector(".timer h1").style.color = "red";
+            document.querySelector(".digits").style.color = "red";
             clearInterval(timerEvent.interval);
           }
         }, 10);
-   
     }
 
     onReset(){
         this.timer = 0;
-        document.querySelector(".timer h1").textContent = this.timer.toFixed(2);
+        document.getElementById("secondTens").textContent = "-";
+        document.getElementById("secondOnes").textContent = "-";
+        document.getElementById("msHundreds").textContent = "-";
+        document.getElementById("msTens").textContent = "-";
+
         this.startButton.disabled = false;
-        document.querySelector(".timer h1").style.color = "black";
+        document.querySelector(".digits").style.color = "black";
         clearInterval(this.interval);
     }
   }
